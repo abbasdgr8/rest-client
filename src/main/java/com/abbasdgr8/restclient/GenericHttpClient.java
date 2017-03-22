b3208fb2967839f8873d0130168ece2b3f5d95c4
Abbas Attarwala <abbas.attarwala@oracle.com>
2017-03-22 13:39:38 +0530

    Added class and interface skeletons

---
+++ src/main/java/com/abbasdgr8/restclient/GenericHttpClient.java
@@ -0,0 +1,27 @@
+package com.abbasdgr8.restclient;
+
+/**
+ *
+ * @author Abbas Attarwala
+ */
+public class GenericHttpClient {
+    
+    public void get() {
+    
+        
+    
+    }
+    
+    public void post() {}
+    
+    public void put() {}
+    
+    public void delete() {}
+    
+    public void options() {}
+    
+    public void trace() {}
+    
+    public void patch() {}
+    
+}
--- src/main/java/com/abbasdgr8/restclient/RestClient.java
+++ src/main/java/com/abbasdgr8/restclient/RestClient.java
@@ -1,13 +1,19 @@
 package com.abbasdgr8.restclient;
 
 /**
- * A light weight and easy to use rest client library 
- * built on top of the Apache HTTP Client library.
+ * A light weight and easy to use rest client utility 
+ * built on top of the Apache HTTP Commons library.
  *
  * @author Abbas Attarwala
  */
 public class RestClient {
     
+    public void getResource() {}
     
+    public void createResource() {}
+    
+    public void updateResource() {}
+    
+    public void deleteResource() {}
     
 }
